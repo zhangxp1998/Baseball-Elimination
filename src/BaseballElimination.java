fb70089f65c93f1c33619ae1c843ef91b1f3e0c2
Kelvin Zhang <zhangxp1998@gmail.com>
2017-02-09 16:57:44 -0500

    Sets up the project

---
+++ src/BaseballElimination.java
@@ -0,0 +1,51 @@
+
+public class BaseballElimination
+{
+	public BaseballElimination(String filename) // create a baseball division
+												// from given filename in format
+												// specified below
+	{
+	}
+
+	public int numberOfTeams() // number of teams
+	{
+	}
+
+	public Iterable<String> teams() // all teams
+	{
+	}
+
+	public int wins(String team) // number of wins for given team
+	{
+	}
+
+	public int losses(String team) // number of losses for given team
+	{
+	}
+
+	public int remaining(String team) // number of remaining games for given
+										// team
+	{
+	}
+
+	public int against(String team1, String team2) // number of remaining games
+													// between team1 and team2
+	{
+	}
+
+	public boolean isEliminated(String team) // is given team eliminated?
+	{
+	}
+
+	public Iterable<String> certificateOfElimination(String team) // subset R of
+																	// teams
+																	// that
+																	// eliminates
+																	// given
+																	// team;
+																	// null if
+																	// not
+																	// eliminated
+	{
+	}
+}
